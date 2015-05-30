d1d86f16ce6b30f0d13e70505aa68fa79082b2a9
Travis Ralston <travpc@gmail.com>
2015-05-29 19:38:23 -0600

    Include example attempt at socket.io

---
+++ www/attempt/serve.py
@@ -0,0 +1,55 @@
+from socketio import socketio_manage
+from socketio.server import SocketIOServer
+from socketio.namespace import BaseNamespace
+from socketio.mixins import BroadcastMixin
+
+class CPUNamespace(BaseNamespace, BroadcastMixin):
+    def recv_connect(self):
+        print("It works")
+        self.emit("test", "Hello")
+        def sendcpu():
+            self.emit('test', "hello")
+        self.spawn(sendcpu)
+
+
+class Application(object):
+    def __init__(self):
+        self.buffer = []
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO'].strip('/') or 'index.html'
+
+        if path.startswith('static/') or path == 'index.html':
+            try:
+                data = open(path).read()
+            except Exception:
+                return not_found(start_response)
+
+            if path.endswith(".js"):
+                content_type = "text/javascript"
+            elif path.endswith(".css"):
+                content_type = "text/css"
+            elif path.endswith(".swf"):
+                content_type = "application/x-shockwave-flash"
+            else:
+                content_type = "text/html"
+
+            start_response('200 OK', [('Content-Type', content_type)])
+            return [data]
+
+        if path.startswith("socket.io"):
+            socketio_manage(environ, {'/cpu': CPUNamespace})
+        else:
+            return not_found(start_response)
+
+
+def not_found(start_response):
+    start_response('404 Not Found', [])
+    return ['<h1>Not Found</h1>']
+
+
+if __name__ == '__main__':
+    print 'Listening on port http://0.0.0.0:8080 and on port 10843 (flash policy server)'
+    SocketIOServer(('0.0.0.0', 8080), Application(),
+        resource="socket.io", policy_server=True,
+        policy_listener=('0.0.0.0', 10843)).serve_forever()
