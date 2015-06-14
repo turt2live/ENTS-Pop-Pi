49bf6c05f3c0c110dd8de5542038c29ad11903e8
turt2live <travpc@gmail.com>
2015-06-14 16:02:05 -0600

    Add setup script

---
+++ pi/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ents-poppi',
+      version='1.0',
+      description='ENTS RaspberryPi-controller pop machine',
+      url='https://github.com/turt2live/ENTS-Pop-Pi',
+      install_requires=[
+          'RPi.GPIO',
+          'gevent-socketio'
+      ],
+      zip_safe=False)
